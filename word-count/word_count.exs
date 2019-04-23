0f17e8e3e4b280825053b5b0495f94ee668ece61
Gabriel Packer <ggkpacker@gmail.com>
2019-04-22 23:01:24 -0300

    Improve quality

--- word-count/word_count.exs
+++ word-count/word_count.exs
@@ -1,5 +1,3 @@
-require IEx
-
 defmodule Words do
   @doc """
   Count the number of words in the sentence.
@@ -8,15 +6,11 @@ defmodule Words do
   """
   @spec count(String.t()) :: map
   def count(sentence) do
-    words =
-      sentence
+    sentence
       |> String.downcase()
-      |> String.split(~r/([^-\w]|_)/u, trim: true)
-
-    word_count = Map.new(words, &{&1, 0})
-
-    Enum.reduce(words, word_count, fn word, acc ->
-      Map.update!(acc, word, &(&1 + 1))
-    end)
+      |> String.split(~r/[^[:alnum:]-]/u, trim: true)
+      |> Enum.reduce(%{}, fn word, acc ->
+        Map.update(acc, word, 1, &(&1 + 1))
+      end)
   end
 end
